dffbf1956dbe9a3d48dc05c0831a9da82938b76a
MuhAgungN <89763501+MuhAgungN@users.noreply.github.com>
2022-12-20 22:19:26 +0700

    ini koneksi/config

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+
+    $host       = "localhost";
+    $username   = "root";
+    $password   = "";
+    $database   = "airlines_database";
+    $koneksi    = mysqli_connect($host,$username,$password,$database);
+
+    if(!$koneksi){
+        die("koneksi gagal");
+    }
+
+?>
